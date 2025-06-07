39823592408bbfb71f4b56d42aee1445bbd516c8
gokkerz <luchijdra37@googlemail.com>
2025-06-07 20:40:11 +0000

    toinkkk

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -158,8 +158,8 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'scroll-right': 'scroll-right 10s linear infinite',
-        'scroll-left': 'scroll-left 10s linear infinite',
+        'scroll-right': 'scroll-right 20s linear infinite',
+        'scroll-left': 'scroll-left 20s linear infinite',
         pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
         'fade-in': 'fade-in 0.5s ease-out',
         float: 'float 3s ease-in-out infinite',
